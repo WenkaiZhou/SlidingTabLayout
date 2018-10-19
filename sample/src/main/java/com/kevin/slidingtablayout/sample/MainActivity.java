d47be3c6a901c8a0aab8ed757df0639875b93295
zhouwenkai <zhouwenkai@baidu.com>
2018-10-19 16:24:17 +0800

    Add slidingtablayout module

---
+++ sample/src/main/java/com/kevin/slidingtablayout/sample/MainActivity.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2018 Kevin zhou
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.kevin.slidingtablayout.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * MainActivity
+ *
+ * @author zwenkai@foxmail.com, Created on 2018-10-19 16:13:02
+ *         Major Function：<b>MainActivity</b>
+ *         <p/>
+ *         Note: If you modify this class please fill in the following content as a record.
+ * @author mender，Modified Date Modify Content:
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
